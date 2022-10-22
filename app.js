1a6f7d717cf019b3c1ce3a5635be7bca54b7894d
ossianhempel <hemposse@hotmail.com>
2022-10-22 15:32:24 +0200

    Edit html board and add styling

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 
 
+let gameBoardArray = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+    []
+]
 
 
 
 
-
-gridCells = document.querySelectorAll('.grid-container-cell');
+gridCells = document.querySelectorAll('[data-cell]');
 gridCells.forEach(cell => {
     cell.addEventListener('click', (e) => {
+        // instead of setting text-content to X we should
+        // fill the array with X and then render the board
+        // to reflext the array
+        // cell.setAttribute('class', 'x')
         cell.textContent = 'X';
     })
 });
+
+
+
