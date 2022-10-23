f64a6767fe06c1dcbc8ac245284c506f3827c62b
ossianhempel <hemposse@hotmail.com>
2022-10-23 11:12:27 +0200

    Add function to show/hide modal

--- app.js
+++ app.js
@@ -7,19 +7,65 @@ let gameBoardArray = [
     []
 ]
 
+// TODO eventListener to the modal
+const modal = document.querySelector('#modal');
+const restartButton = document.querySelector('#restartButton');
+
+restartButton.addEventListener('click', () => {
+    showModal();
+});
+
+
+function showModal() {
+    if (modal.style.display === 'none') {
+        modal.style.display = 'block';
+    } else {
+        modal.style.display = 'none';
+    }
+};
+
+
+// TODO set win conditions
+
+
+function handleClick(e) {
+    // TODO add x/o to board either directly or through an array that renders the board + add x/o to the cell's class 
+    // TODO switch player (change class on board)
+    // TODO check if someone's won
+        // * If so - trigger Modal with appropriate winning message
+};
+
+function addMark(e) {
+    if (circleTurn === true) {
+
+    } else {
+        e.target.textContent
+    }
+    
+    e.target.textContent 
+}
 
 
 
-gridCells = document.querySelectorAll('[data-cell]');
+const gridCells = document.querySelectorAll('[data-cell]');
 gridCells.forEach(cell => {
-    cell.addEventListener('click', (e) => {
+    cell.addEventListener('click', handleClick, {once: true})
         // instead of setting text-content to X we should
         // fill the array with X and then render the board
         // to reflext the array
         // cell.setAttribute('class', 'x')
-        cell.textContent = 'X';
-    })
+        
 });
+
+
+
+// TODO Modal 
+    // * Add eventlistener to button
+    // * functionality to reset the board 
+
+
+// TODO Add AI using MinMax-algorithm
+
 
 
 
