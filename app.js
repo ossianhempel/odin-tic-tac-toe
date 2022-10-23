50d139ea627f227b8bf6b6851695167065905e3b
ossianhempel <hemposse@hotmail.com>
2022-10-23 17:08:43 +0200

    Add functions to add appropriate mark to the board and switch player

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let gameBoardArray = [
 // TODO eventListener to the modal
 const modal = document.querySelector('#modal');
 const restartButton = document.querySelector('#restartButton');
+const board = document.querySelector('#board');
 
 restartButton.addEventListener('click', () => {
     showModal();
@@ -25,26 +26,46 @@ function showModal() {
 };
 
 
+let circleTurn = true;
+
 // TODO set win conditions
 
 
 function handleClick(e) {
-    // TODO add x/o to board either directly or through an array that renders the board + add x/o to the cell's class 
+    
+    // TODO add x/o to board either directly or through an array that renders the board + add x/o to the cell's class
+    addMark(e, circleTurn);
+    
     // TODO switch player (change class on board)
+    switchPlayer(circleTurn);
+
     // TODO check if someone's won
         // * If so - trigger Modal with appropriate winning message
 };
 
 function addMark(e) {
-    if (circleTurn === true) {
 
+    if (circleTurn === true) {
+        e.target.textContent = 'O';
+        e.target.setAttribute('class', 'board-cell o')
     } else {
-        e.target.textContent
+        e.target.textContent = 'X';
+        e.target.setAttribute('class', 'board-cell x')
     }
-    
-    e.target.textContent 
 }
 
+function switchPlayer() {
+    if (circleTurn === true) {
+        circleTurn = false;
+        board.setAttribute('class', 'board x');
+
+    } else {
+        circleTurn = true;
+        board.setAttribute('class', 'board o');
+
+    }
+    // return circleTurn;
+}
 
 
 const gridCells = document.querySelectorAll('[data-cell]');
