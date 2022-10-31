c93f72a3952586d6aa51e6fa3a55eeeec1a1621f
ossianhempel <hemposse@hotmail.com>
2022-10-31 19:00:41 +0100

    Create gameOver function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('#myForm');
 const modal = document.querySelector('#modal');
+const restartModal = document.querySelector('#restartModal');
+restartModal.style.display = 'none';
 const restartButton = document.querySelector('#restartButton');
 const backToMenuButton = document.querySelector('#backToMenuButton');
 
@@ -10,7 +12,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
-    showModal();
+    showModal('modal');
     InitializeGame(data);
 });
 
@@ -40,8 +42,9 @@ function addEventListenerstoButtons(data) {
     const dataObject = data;
     restartButton.addEventListener('click', () => {
         resetBoard(dataObject);
+        showModal('restartModal');
     })
-    
+
     // TODO fix this one!!! similar to the one above 
     backToMenuButton.addEventListener('click', () => {
         console.log('works!')
@@ -64,12 +67,20 @@ const InitializeGame = (data) => {
 
 
 
-// show/hide modal
-function showModal() {
-    if (modal.style.display === 'none') {
-        modal.style.display = 'block';
-    } else {
-        modal.style.display = 'none';
+// show/hide modal based on which modal is passed as an argument
+function showModal(whichModal) {
+    if (whichModal === 'modal') {
+        if (modal.style.display === 'none') {
+            modal.style.display = 'block';
+        } else {
+            modal.style.display = 'none';
+        }
+    } else if (whichModal === 'restartModal') {
+        if (restartModal.style.display === 'none') {
+            restartModal.style.display = 'block';
+        } else {
+            restartModal.style.display = 'none';
+        }
     }
 };
 
@@ -116,12 +127,26 @@ function addMark(e, data) {
 function checkIfWin(e, data) {
     if (data.round === 8) {
         data.gameOver = true;
+        gameOver(e, data, 'draw');
+    } else if () {
+
+    } else if () {
+
     }
-    console.log(data.gameOver);
 }
 
-function gameOver(e, data) {
-
+// TODO IN-PROGRESS
+function gameOver(e, data, winner) {
+    if (winner === 'p1') {
+        document.querySelector('[data-winning-message]').textContent = `${data.p1} WINS!`;
+        showModal('restartModal');
+    } else if (winner === 'p2') {
+        document.querySelector('[data-winning-message]').textContent = `${data.p2} WINS!`;
+        showModal('restartModal');
+    } else if (winner === 'draw') {
+        document.querySelector('[data-winning-message]').textContent = 'DRAW!';
+        showModal('restartModal');
+    }
 }
 
 // Swith player between x/o
@@ -143,24 +168,21 @@ function switchPlayer(e, data) {
     
 }
 
-//* Reset the board and start from the beginning with the same players
-// THE PROBLEM HERE IS THAT IT CREATES A NEW OBJECT INSTEAD OF USING THE SAME AS THE OTHER!
-// WHY? BECAUSE THIS EVENT LISTENER ISN'T CALLED 
-// IN AN ENVIRONMENT WHERE THE ORIGINAL OBJECT IS ALREADY INITIALIZED! 
+// Reset the board and start from the beginning with the same players
 function resetBoard(data) {
     initializaVariables(data);
  
     const cells = document.querySelectorAll('[data-cell]');
     cells.forEach(cell => cell.textContent = '');
-    console.log(data);
+
+    document.querySelector('[data-winning-message]').textContent = '';
+
 }
 
 // Iterate data.round
 function nextRound(data) {
     data.round++;
 }
-
-
 
 
 
